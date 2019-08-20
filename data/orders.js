f0aaa7a28523a4c51c52216e78623703bcecd9c8
Alex Tong <tongsalex@gmail.com>
2019-08-20 12:45:02 -0400

    change to floats

--- data/orders.js
+++ data/orders.js
@@ -1,21 +1,21 @@
 const orders = [
   {
     "itemName": "coke",
-    "price":25.00,
+    "price":25.15,
     "receiptId": 2,
     "userId": 2,
     // "paid": false
   },
   {
     "itemName": "sprite",
-    "price":30.00,
+    "price":30.30,
     "receiptId": 1,
     "userId": 1,
     // "paid": false
   },
   {
     "itemName": "ginger ale",
-    "price":24.00,
+    "price":24.60,
     "receiptId": 1,
     "userId": 1,
     // "paid": false
