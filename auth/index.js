12980b3b053daa48ffa1778f840a7b382b7b2f2f
Alex Tong <tongsalex@gmail.com>
2019-08-20 20:55:09 -0400

    modified login route to return data once logged in

--- auth/index.js
+++ auth/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { User } = require("../database/models");
+const { User, Receipt, Order } = require("../database/models");
 
 router.post("/login", async (req, res, next) => {
   
   try {
-    const user = await User.findOne({ where: { username: req.body.username } });
+    const user = await User.findOne({ 
+      where: { 
+        username: req.body.username 
+      },
+      // attributes: ['id', 'firstName', 'lastName', 'username'],
+      // include: 
+      // [{ model: Receipt,
+      //   include: [{ model: Order }]},
+      // {model: Order}]
+    })
+
     if (!user) {
       res.status(401).send("Wrong username and/or password");
     }
@@ -13,9 +23,20 @@ router.post("/login", async (req, res, next) => {
       res.status(401).send("Wrong username and/or password");
     }
     else {
-      req.login(user, err => (err ? next(err) : res.json(user)));
+      let foundUser = await User.findOne({ 
+        where: { 
+          username: req.body.username 
+        },
+        attributes: ['id', 'firstName', 'lastName', 'username'],
+        include: 
+        [{ model: Receipt,
+          include: [{ model: Order }]},
+        {model: Order}]
+      })
+      req.login(user, err => (err ? next(err) : res.json(foundUser)));
     }
   }
+
   catch (err) {
     next(err);
   }
