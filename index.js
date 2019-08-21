2b112a89beff3e56aaaeb2ae51f2e2e8c04050b5
Alex Tong <tongsalex@gmail.com>
2019-08-21 11:01:55 -0400

    removed cookieparser

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require("express-session");
 
 const path = require('path');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
 const compression = require('compression');
@@ -64,7 +64,6 @@ const configureApp = () => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(compression());
-  app.use(cookieParser());
   app.use(cors());
 
   app.use(
