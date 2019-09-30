623c3a5f42d0f2e658c8d686eb4d1532d0997c07
thomas-westfall <thomas.westfall64@myhunter.cuny.edu>
2019-09-30 13:55:59 -0400

    add header to route & users

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
 
 // Subrouters;
 const usersRouter = require('./users');
@@ -7,9 +8,16 @@ const receiptsRouter = require('./receipts');
 const ordersRouter = require('./orders');
 
 // Mount our subrouters to assemble our apiRouter;
+router.use(cors());
 router.use('/users', usersRouter);
 router.use('/receipts', receiptsRouter);
 router.use('/orders', ordersRouter);
+
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Error handling middleware;
 router.use((req, res, next) => {
